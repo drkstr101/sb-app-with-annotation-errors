bf308df7f28d69953c18a23abaa4a5510c8cc91a
Aaron R Miller <aaron@stackbit.com>
2022-06-28 22:46:16 -0700

    add prebuild script

--- bin/dev.js
+++ bin/dev.js
@@ -1,23 +1,22 @@
 #!/usr/bin/env node
 
-const open = require("open");
-const { exec } = require("child_process");
+const open = require('open');
+const { exec } = require('child_process');
 
-const command = "npm run stackbit dev";
+// Use our own local copy of stackbit cli (for testing)
+const command = 'node ./stackbit/packages/stackbit-cli/dist/stackbit.js';
 
 const devProcess = exec(command, (err) => {
-  console.error(`Failed to execute ${command}`, err);
+    console.error(`Failed to execute ${command}`, err);
 });
 
-devProcess.stdout.on("data", (data) => {
-  const urlMatching = data.match(
-    /(https:\/\/app\.stackbit\.com\/local\/.*?)\s/
-  );
+devProcess.stdout.on('data', (data) => {
+    const urlMatching = data.match(/(https:\/\/app\.stackbit\.com\/local\/.*?)\s/);
 
-  if (urlMatching) {
-    console.log(`Opening local editor in browser (${urlMatching[1]})`);
-    open(urlMatching[1]);
-  } else {
-    console.log(data.trim());
-  }
+    if (urlMatching) {
+        console.log(`Opening local editor in browser (${urlMatching[1]})`);
+        open(urlMatching[1]);
+    } else {
+        console.log(data.trim());
+    }
 });
